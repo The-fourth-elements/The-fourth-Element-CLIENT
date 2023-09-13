6469412df0a6aa29bc0c3a2dc545708162604ade
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-09-13 17:16:44 -0300

    update landing

---
+++ src/app/landing/page.jsx
@@ -0,0 +1,110 @@
+import './styles.scss';
+const Landing = () => {
+	return (
+		<div>
+			<div>
+				<div>
+					<img alt='' src='/undefined6.png' />
+					<div>
+						<p>TU ENTRENAMIENTO</p>
+						<p> MENTAL Y CEREBRAL</p>
+						<p>PARA POTENCIAR</p>
+						<p>TU DESEMPEÑO</p>
+						<p>DEPORTIVO</p>
+					</div>
+				</div>
+				<div>
+					<div>
+						<p>¡Bienvenidos a <h3> The Fourth Element! </h3>  Durante 10</p>
+						<p>semanas explorarás una plataforma educativa que</p>
+						<p>fusiona <h3> teoría y práctica </h3> en un enfoque único.</p>
+						<p>Fundamentado en datos científicos, nuestro programa</p>
+						<p>se nutre de la biología cerebral, neurociencias y</p>
+						<p>psicología deportiva cognitivo-conductual.</p>
+					</div>
+				
+				</div>
+				<img alt='' src='/undefined.png' />
+				<div>
+					<img alt='' src='/undefined1.png' />
+					<div>Get started (free trial)</div>
+				</div>
+				<div>
+					<img alt='' src='/undefined2.png' />
+					<div>Grupos</div>
+				</div>
+				<div>
+					<div>Take a tour</div>
+					<img alt='' src='/undefined3.png' />
+					<img alt='' src='/undefined4.png' />
+					<img alt='' src='/undefined5.png' />
+				</div>
+				<div>
+					<div>The Fourth Element</div>
+					<div>
+						<p> está diseñado para...</p>
+						<p></p>
+						<p>Cualquier atleta comprometido con el alto</p>
+						<p>rendimiento, ya sea en disciplinas individuales o de</p>
+						<p>equipo. También, es una oportunidad valiosa para</p>
+						<p>aquellos profesionales que rodean a los deportistas,</p>
+						<p>como entrenadores, preparadores físicos, managers,</p>
+						<p>kinesiólogos, nutricionistas, fisioterapeutas,</p>
+						<p>psicólogos, y más.</p>
+					</div>
+				</div>
+
+				<div>
+					<img alt='' src='/undefined7.png' />
+					<div>
+						<p>The Fourth Element</p>
+					</div>
+					<img alt='' src='/undefined8.png' />
+					<div>
+						<img alt='' src='/undefined9.png' />
+						<div>Quienes somos</div>
+						<div>Testimonios</div>
+						<div>Acerca</div>
+						<div>Precios</div>
+						<div>Login</div>
+						<div>SignUp</div>
+					</div>
+				</div>
+				<img alt='' src='/undefined10.png' />
+				<div />
+				<div>
+					<div>
+						<p>Partners</p>
+						<p>&nbsp;</p>
+						<p>Media</p>
+					</div>
+				</div>
+				<div>
+					<div>
+						<p>Email Us</p>
+						<p>&nbsp;</p>
+						<p>Coaches’ Guide</p>
+					</div>
+				</div>
+				<div>
+					<div>
+						<p>{`Stay up to date with Train `}</p>
+						<p>the Mind®</p>
+					</div>
+				</div>
+				<div>Your email address</div>
+				<div />
+				<div>Get Updates</div>
+				<div>
+					<p>{`facebook        twitter        instagram        linkedin `}</p>
+					<p>&nbsp;</p>
+					<p>{` `}</p>
+				</div>
+				<div>Revoke consent</div>
+				<div>© Train the mind 2023</div>
+			</div>
+		</div>
+	);
+};
+
+export default Landing;
