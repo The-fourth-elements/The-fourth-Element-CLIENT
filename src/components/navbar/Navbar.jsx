30c3e6d4a052dc6566cb52aec5060fe088e3de69
NONHIGH <angelones_24@hotmail.com>
2023-09-13 15:39:22 -0300

    nonhigh: none

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,36 @@
 import Link from "next/link"
+import './styles.scss'
 
 const Navbar = ()=> {
     const routes = [
-        {label: 'Home',route: '/'},
-        {label: 'Login', route: '/login'}
+        {label: 'Home',route: '/', className: ''},
+        {label: 'Login', route: '/login', className: 'gold'},
+        {label: 'SignUp', route: '/register', className: ''}
+
         
     ]
     return (
-        <nav>
-            <ul>
-            {routes.map(({ label, route }) => {
+        <nav className="Navbar">
+          <Link href='/'>
+            {/* logo */}
+            home
+          </Link>
+            <ul className="Navbar__ul">
+            {routes.map(({ label, route, className }) => {
               return (
-                <li key={route}>
-                  <Link href={route}>
+                <li key={route} className="Navbar__ul--li">
+                  <Link href={route} className={'Navbar__ul--li-' +className}>
                     {label}
                   </Link>
                 </li>
               )
             })}
           </ul>
+          <ul>
+            <li>holi</li>
+            <li>holi</li>
+          </ul>
+          
         </nav>
     )
 }
