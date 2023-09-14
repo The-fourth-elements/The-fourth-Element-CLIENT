a0add8dcc433eb4a5471ae248ea825e89d5d710f
Kyrua <ang02aezs@gmail.com>
2023-09-13 23:26:48 -0600

    signup sin terminar

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,10 +1,11 @@
 
-const Sign = ( ) => {
+const SignUp = ( ) => {
     return (
         <div>
             <h1>Holiwi</h1>
+            <h2>este es el signup</h2>
         </div>
     )
 }
 
-export default Sign
+export default SignUp;
