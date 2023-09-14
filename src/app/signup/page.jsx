d20328e87c84cb44b99a39f50e36b25189cd5ed4
Kyrua <ang02aezs@gmail.com>
2023-09-14 00:59:09 -0600

    calle al aaron

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,8 +2,10 @@
 const SignUp = ( ) => {
     return (
         <div>
-            <h1>Holiwi</h1>
+            <h1>Hol</h1>
             <h2>este es el signup</h2>
+            <h2>hola soy goku </h2>
+            <h3>sos un hijo de remil aaron</h3>
         </div>
     )
 }
