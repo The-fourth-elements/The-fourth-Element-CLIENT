3eeda5f4b0bd7bd3fd5dbb993f693921e4a82adc
NONHIGH <angelones_24@hotmail.com>
2023-09-15 01:38:00 -0300

    nonhigh: implement sm devices navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,44 @@
-import Link from "next/link"
-import './styles.scss'
-import Image from "next/image"
-import logo from "../../../public/logo.svg"
+import Link from 'next/link';
+import './styles.scss';
+import Image from 'next/image';
+import logo from '../../../public/logo.svg';
+import burger from '../../assets/svg/burger.svg';
+import x from '../../assets/svg/x-mark.svg'
 
-const Navbar = ()=> {
-    const routes = [
-      {label: 'Home',route: '/', className: ''},
-      {label: 'About Us', route: '/about', className: ''},
-      {label: 'Login', route: '/login', className: 'gold'},
-      {label: 'SignUp', route: '/signup', className: ''},
-
-        
-    ]
-    return (
-        <nav className="Navbar">
-          <Link href='/' className="Navbar__logo">
-            <Image src={logo}></Image>
-          </Link>
-            <ul className="Navbar__ul">
-            {routes.map(({ label, route, className }) => {
-              return (
-                <li key={route} className="Navbar__ul--li">
-                  <Link href={route} className={'Navbar__ul--li-' +className}>
-                    {label}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
-    )
-}
+const Navbar = () => {
+	const routes = [
+		{ label: 'Home', route: '/', className: '' },
+		{ label: 'About Us', route: '/about', className: '' },
+		{ label: 'Login', route: '/login', className: 'gold' },
+		{ label: 'SignUp', route: '/signup', className: '' },
+	];
+	return (
+		<>
+			<nav className='Navbar'>
+				<div className='Navbar-div'>
+					<Link href='/' className='Navbar__logo'>
+						<Image src={logo}></Image>
+					</Link>
+          <input type="checkbox" id="check"/>
+					<label htmlFor="check" className='burger'>
+						<Image src={burger} alt='burger' id='burger' />
+						<Image src={x} alt='x-mark' id='x' />
+					</label>
+					<ul className='Navbar__ul'>
+						{routes.map(({ label, route, className }) => {
+							return (
+								<li key={route} className='Navbar__ul--li'>
+									<Link href={route} className={'Navbar__ul--li-' + className}>
+										{label}
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+};
 
 export default Navbar;
