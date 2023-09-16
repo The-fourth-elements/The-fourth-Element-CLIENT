0ba1174414861c364d57749ba0ce8d183ea96b4e
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-09-16 15:20:30 -0300

    landing desktop and tablet
    responsive  desktop and tablet

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,33 @@
-const Home = ()=>{
-    
-    return (
-        <>
-        <h1>esta es la main page XD asddas</h1>
-        </>
-    )
-}
+'use client';
 
-export default Home;
+import LandingDesktop from './landing/LandingDesktop';
+import LandingMobile from './landing/LandingMobile';
+
+import { useEffect, useState } from 'react';
+import './styles.scss';
+const Landing = () => {
+	const [mobile, setMobile] = useState(undefined);
+
+	useEffect(() => {
+		const updateMobile = () => {
+			setMobile(window.innerWidth < 766 ? true : false);
+		};
+
+		updateMobile();
+		window.addEventListener('resize', updateMobile);
+		return () => {
+			window.removeEventListener('resize', updateMobile);
+		};
+	}, []);
+
+	return typeof mobile !== 'undefined' ? (
+		mobile ? (
+			<LandingMobile />
+
+		) : (
+			<LandingDesktop />
+		)
+	) : null;
+};
+
+export default Landing;
