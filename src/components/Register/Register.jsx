e1541826069dbc1ced28cd9312c878f3a7c0478a
Kyrua <ang02aezs@gmail.com>
2023-09-16 03:15:22 -0600

    signup con diseño en progreso

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,33 +1,25 @@
 "use client"
-import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";import * as Yup from "yup"
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage} from "formik";
 import "./styles.scss"
+import { handleSubmit } from "./handlers";
+import { registerSchema, initialValues } from "./validations";
+import { useRouter } from "next/navigation";
+import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
+
 
 const Register = () => {
-  const initialValues = {
-    username: "",
-    email: "",
-    password: "",
-    repeatPassword: "",
-  };
 
-  const registerSchema = Yup.object({
-    username: Yup.string().required().min(5).max(20).matches(/^[A-Za-z0-9_-]+$/, "The user name can only contain letters, numbers, underscores (_) and hyphens (-)."),
-    email: Yup.string().email().required(),
-    password: Yup.string().required().min(8).max(25),
-    repeatPassword: Yup.string()
-    .required()
-    .oneOf([Yup.ref('password')], 'Passwords must match')
-
-  });
+  const router = useRouter()
+  const [country, setCountry] = useState("");
+  const [region, setRegion] = useState("");
 
   return (
     <div className="Div">
   <Formik
     initialValues={initialValues}
-    validationSchema={registerSchema}
-    onSubmit={() => {
-      alert("registro exitoso");
-    }}
+    validationSchema={registerSchema(country, region)}
+    onSubmit={(values) => handleSubmit(values, router, country, region)}
   >
     {({ errors }) => (
       <Form>
@@ -88,6 +80,36 @@ const Register = () => {
             className="Main__Form--error"
           />
         </div>
+
+        <div className="Main__Form--group">
+              <label htmlFor="country" className="Main__Form--label">
+                Country:
+              </label>
+              <CountryDropdown
+                name="country"
+                value={country}
+                onChange={(val) => {
+                  setCountry(val);
+                  setRegion('');
+                }}
+                className="Main__Form--input"
+              />
+            </div>
+
+            {country && (
+              <div className="Main__Form--group">
+                <label htmlFor="state" className="Main__Form--label">
+                  State/Region:
+                </label>
+                <RegionDropdown
+                  country={country}
+                  value={region}
+                  onChange={(val) => setRegion(val)}
+                  className="Main__Form--input"
+                />
+                <ErrorMessage name="state" component="p" className="Main__Form--error" />
+              </div>
+            )}
 
         <button type="submit" className="Main__Form--button">
           Register
