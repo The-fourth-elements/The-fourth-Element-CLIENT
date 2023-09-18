8d7bbc053b764305164e5ac8985928ebb5eb1df3
NONHIGH <angelones_24@hotmail.com>
2023-09-18 14:48:36 -0300

    jD

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,6 @@
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 
-// 'use client';
-
 import { Inter } from 'next/font/google';
 import '../styles/globals.scss';
 import { AuthContextProvider } from './context/authContext';
