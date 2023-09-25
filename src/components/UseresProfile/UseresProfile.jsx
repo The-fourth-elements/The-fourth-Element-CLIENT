2271d874cf74bffc027c513b27b90263721b3744
Kyrua <ang02aezs@gmail.com>
2023-09-25 06:11:03 -0600

    le falle

---
+++ src/components/UseresProfile/UseresProfile.jsx
@@ -0,0 +1,58 @@
+import "./styles.scss"
+import { useEffect } from "react";
+import { useUsersStore } from "@/zustand/store/usersStore";
+const UseresProfile = ({params}) => {
+	const{getUsers, usersFilter} = useUsersStore()
+
+	useEffect(() => {
+		getUsers()
+	}, []);
+
+	const user = usersFilter.find((user) => user.name === params.name)
+    return (
+		<article>
+			 
+			{user.name && Object.keys(user).length > 0 ? (
+				<Card className='main'>
+					<CardHeader className='elHeader'>
+						<h1>Name: {user.name}</h1>
+						{user.profile_img ? <Image
+							src={user.profile_img}
+							alt={user.name}
+						/>: <Image
+						src='https://cdn.pnghd.pics/data/862/user-profile-png-15.png'
+						alt={user.name}
+					/>}
+					</CardHeader>
+					<CardBody className='elBody'>
+						<h2>Email: {user.email}</h2>
+						{user.role === 0 ? (
+							<h2>Plan: Free Plan</h2>
+						) : user.rol === 1 ? (
+							<h2>Plan: Pay Plan</h2>
+						) : (
+							<h2>Plan: Admin </h2>
+						)}
+						<h2>Country: {user.nationality}</h2>
+						<h2>City: {user.city}</h2>
+						{/* <h2>Registration date: {detail.createdAt}</h2>  */}
+						<h2>
+							Registration date:{' '}
+							{new Date(user.createdAt).toLocaleDateString()}
+						</h2>
+					</CardBody>
+				</Card>
+			) : (
+				<div className='centered'>
+					<CircularProgress
+						className='loading'
+						label='Loading...'
+						color='warning'
+					/>
+				</div>
+			)}
+		</article>
+	);
+}
+
+export default UseresProfile;
--- src/middleware.js
+++ src/middleware.js
@@ -1,25 +1,42 @@
 import { NextResponse } from 'next/server';
+import nextAuth from 'next-auth';
 
 export const middleware = async request => {
     const session = request.cookies.get('next-auth.session-token');
+    const user = await nextAuth.getSession(session); // aqui deberia obtener la sesion del usuario actual pero siempre me da vacio
+    // es como si ningun usuario ingresara
+    const role = user?.user?.role;// entonces ya no tiene propiedades
 
-    if (session && request.nextUrl.pathname === '/auth') {
-        // El usuario está autenticado y está intentando acceder a la página de autenticación,
-        // redirigirlo a la página de curso.
-        const url = request.nextUrl.clone();
-        url.pathname = '/dashboard';
-        return NextResponse.redirect(url);
+    const pathname = request.nextUrl?.pathname ?? '';
+
+   
+    if (!pathname) {
+        return NextResponse.redirect('/');
     }
-    if (session && request.nextUrl.pathname === '/dashboard') {
-        const url = request.nextUrl.clone();
+
+    if (session && pathname === '/auth') {
+        if (role <= 1) {
+            
+            const url = request.nextUrl?.clone();
+            url.pathname = '/course';
+            return NextResponse.redirect(url);
+        } else {
+            
+            const url = request.nextUrl?.clone();
+            url.pathname = '/dashboard';
+            return NextResponse.redirect(url);
+        }
+    }
+    if (session && pathname === '/dashboard') {
+        const url = request.nextUrl?.clone();
         //verificar el rol
         url.pathname = '/course';
         return NextResponse.redirect(url);
     }
 
-    if (!session && request.nextUrl.pathname !== '/auth') {
-        const requestedPage = request.nextUrl.pathname;
-        const url = request.nextUrl.clone();
+    if (!session && pathname !== '/auth') {
+        const requestedPage = pathname;
+        const url = request.nextUrl?.clone();
         url.pathname = '/auth';
         url.search = `p=${requestedPage}`;
         return NextResponse.redirect(url);
@@ -27,6 +44,33 @@ export const middleware = async request => {
 
     return NextResponse.next();
 };
+// export const middleware = async request => {
+//     const session = request.cookies.get('next-auth.session-token');
+
+//     if (session && request.nextUrl.pathname === '/auth') {
+//         // El usuario está autenticado y está intentando acceder a la página de autenticación,
+//         // redirigirlo a la página de curso.
+//         const url = request.nextUrl.clone();
+//         url.pathname = '/dashboard';
+//         return NextResponse.redirect(url);
+//     }
+//     if (session && request.nextUrl.pathname === '/dashboard') {
+//         const url = request.nextUrl.clone();
+//         //verificar el rol
+//         url.pathname = '/course';
+//         return NextResponse.redirect(url);
+//     }
+
+//     if (!session && request.nextUrl.pathname !== '/auth') {
+//         const requestedPage = request.nextUrl.pathname;
+//         const url = request.nextUrl.clone();
+//         url.pathname = '/auth';
+//         url.search = `p=${requestedPage}`;
+//         return NextResponse.redirect(url);
+//     }
+
+//     return NextResponse.next();
+// };
 
 export const config = {
     matcher: ['/course', '/auth', '/dashboard'],
