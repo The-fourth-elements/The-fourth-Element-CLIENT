801b7612658f00950248eeffb8229b40882c3358
NONHIGH <angelones_24@hotmail.com>
2023-09-25 16:45:31 -0300

    ultimio

--- src/app/api/register/route.jsx
+++ src/app/api/register/route.jsx
@@ -6,7 +6,7 @@ export async function POST(request){
     const body = await request.json();
     const {name, email, password} = body;
     //SE CREA EL USUARIO.
-    const response = await postData('http://localhost:3001/auth', body);
+    const response = await postData(`${process.env.API_BACKEND}auth`, body);
     
     return NextResponse.json(response)
 }
--- src/zustand/store/userLoged.js
+++ src/zustand/store/userLoged.js
@@ -1,14 +1,15 @@
 
 import { create } from "zustand";
 
-export const useUserLoged = create ((set, get) => ({
-    userLoged: {name: 0},
-    getUserLoged: () => fetch(`http://localhost:3001/user?id=650fb145cafb2bc5654a13b7`)
-    .then ((response) => response.json())
-    .then ((data) => {
-        set((state) => ({
-            ...state,
-        userLoged: data
-    }))}
-    )
+export const useUserLoged = create((set, get) => ({
+    userLoged: { name: 0 },
+    getUserLoged: () => fetch(`${process.env.API_BACKEND}user?id=650fb145cafb2bc5654a13b7`)
+        .then((response) => response.json())
+        .then((data) => {
+            set((state) => ({
+                ...state,
+                userLoged: data
+            }))
+        }
+        )
 }))
