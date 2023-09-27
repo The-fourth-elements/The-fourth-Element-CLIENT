669dbf91475673e9a0553007b94daf4e1a1e618c
Kyrua <ang02aezs@gmail.com>
2023-09-27 01:39:15 -0600

    unmerge

--- src/helpers/InputField.jsx
+++ src/helpers/InputField.jsx
@@ -4,6 +4,8 @@ import { Input } from '@nextui-org/react';
 const InputFiled = ({ ...props }) => {
 	const [field, meta] = useField(props);
 	const words = field.name.split(/(?=[A-Z])/).join(' ');
+	const { viewPassword } = props;
+
 	return (
 		<>
 			<Input
@@ -13,11 +15,12 @@ const InputFiled = ({ ...props }) => {
 				color={meta.error ? 'danger' : 'default'}
 				{...props}
 				{...field}
-				type={!field.name.includes('assword') ? 'text' : 'password'}
+				type={!field.name.includes("assword") ? "text" : viewPassword ? 'text' : 'password'}
 				autoComplete='false'
 			/>
 			<div className='flex justify-center p-5 modern text-2xl '>
 				{meta.error && <span className='modern bg-primary-700'>{meta.error}</span>}
+				
 			</div>
 		</>
 	);
