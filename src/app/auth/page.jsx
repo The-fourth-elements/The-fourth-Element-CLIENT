28a9bca1383911fe01bcec32e43079ef9212dcb1
NONHIGH <angelones_24@hotmail.com>
2023-09-28 15:22:28 -0300

    cookies fix

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -5,6 +5,11 @@ import Register from '../../components/Register/Register';
 import { useState } from 'react';
 
 const page = () => {
+	const { data: session } = useSession();
+	const id = session?.token?.user?.id;
+	if (id) {
+		setCookie('jsdklfsdjklfdsjfds', id);
+	}
 	const [display, setDisplay] = useState(true);
 	const toogleDisplay = () => {
 		setDisplay(!display);
