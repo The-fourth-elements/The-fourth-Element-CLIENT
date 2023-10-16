f56b89221fc65d33cc9e09ca78828d644862ecd2
NONHIGH <angelones_24@hotmail.com>
2023-10-16 12:01:22 -0300

    nonhigh: zustand return fix and update of user

--- src/components/UserProfile/HeadBodyProfile.jsx
+++ src/components/UserProfile/HeadBodyProfile.jsx
@@ -1,5 +1,13 @@
-"use client"
-import {Card, CardHeader, CardBody, Image, CircularProgress, Button, select } from '@nextui-org/react';
+'use client';
+import {
+	Card,
+	CardHeader,
+	CardBody,
+	Image,
+	CircularProgress,
+	Button,
+	select,
+} from '@nextui-org/react';
 import InputName from './input';
 import { EditIcon } from '@/assets/svg-jsx/EditIcon';
 import { CountrySelect, CitySelect } from './SelectsProfile';
@@ -7,84 +15,204 @@ import { useState } from 'react';
 import { InputAge, InputExp } from './inputExp';
 import { CldUploadButton } from 'next-cloudinary';
 
-export const UserProfileHeader = ({ user, openName, handleChangeName, handleChangePhoto, updateUserName, getNewName, newName, session, openImage, getNewImage, updateUserImage, newImage}) => {
-  const [hasErrors, setHasErrors] = useState(false);
+export const UserProfileHeader = ({
+	user,
+	openName,
+	handleChangeName,
+	handleChangePhoto,
+	updateUserName,
+	getNewName,
+	newName,
+	session,
+	openImage,
+	getNewImage,
+	updateUserImage,
+	newImage,
+}) => {
+	const [hasErrors, setHasErrors] = useState(false);
 
-  // Función para actualizar el estado de errores
-  const handleValidationErrors = (hasErrors) => {
-    setHasErrors(hasErrors);
-  };
-  
-  return(
-    <CardHeader className='elHeader'>
-      {openName ? (
-        <h1>
-          <InputName getNewName={getNewName} name={newName}             handleValidationErrors={handleValidationErrors}/>
-          <Button size="sm" title="back to" color="warning" variant="bordered" onClick={handleChangeName} isIconOnly>
-            ↩
-          </Button>
-          <Button color="warning" variant="bordered" size="sm" onClick={updateUserName} disabled={hasErrors} > Accept </Button>
-        </h1>
-      ) : (
-        <h1>
-          Name: {user?.username}
-          <Button title="Edit  Name" color="warning" variant="bordered" onClick={handleChangeName} size="sm" isIconOnly>
-            <EditIcon />
-          </Button>
-        </h1>
-      )}
-      { user?.profile_img ? (
-        <Image src={user?.profile_img?.secure_url} alt={user?.name} />
-      ) :
-      session?.token?.picture ?(         
-      <Image src={session?.token?.picture} alt="profileImage" />)
-      :
-      (
-        <Image
-          src='https://cdn.pnghd.pics/data/862/user-profile-png-15.png'
-          alt={user?.name}
-        />
-      )}
-      {openImage ? <div><CldUploadButton uploadPreset={process.env.NEXT_PUBLIC_UPLOAD_PRESET}
-							disabled={newImage?.url?.length > 0} onSuccess={getNewImage}/> <Button onClick={updateUserImage}>Accept</Button></div>
-      :<Button isIconOnly color="warning" variant="bordered" title="Edit Photo" onClick={handleChangePhoto}>
-        <EditIcon />
-      </Button>
-      }
-      
-    </CardHeader>
-  )
+	// Función para actualizar el estado de errores
+	const handleValidationErrors = hasErrors => {
+		setHasErrors(hasErrors);
+	};
+
+	return (
+		<CardHeader className='elHeader'>
+			{openName ? (
+				<h1>
+					<InputName
+						getNewName={getNewName}
+						name={newName}
+						handleValidationErrors={handleValidationErrors}
+					/>
+					<Button
+						size='sm'
+						title='back to'
+						color='warning'
+						variant='bordered'
+						onClick={handleChangeName}
+						isIconOnly>
+						↩
+					</Button>
+					<Button
+						color='warning'
+						variant='bordered'
+						size='sm'
+						onClick={updateUserName}
+						disabled={hasErrors}>
+						Accept
+					</Button>
+				</h1>
+			) : (
+				<h1>
+					Name: {user?.username}
+					<Button
+						title='Edit  Name'
+						color='warning'
+						variant='bordered'
+						onClick={handleChangeName}
+						size='sm'
+						isIconOnly>
+						<EditIcon />
+					</Button>
+				</h1>
+			)}
+			{user?.profile_img ? (
+				<Image src={user?.profile_img?.secure_url} alt={user?.name} />
+			) : session?.token?.picture ? (
+				<Image src={session?.token?.picture} alt='profileImage' />
+			) : (
+				<Image
+					src='https://cdn.pnghd.pics/data/862/user-profile-png-15.png'
+					alt={user?.name}
+				/>
+			)}
+			{openImage ? (
+				<div>
+					<CldUploadButton
+						uploadPreset={process.env.NEXT_PUBLIC_UPLOAD_PRESET}
+						disabled={newImage?.url?.length > 0}
+						onSuccess={getNewImage}
+					/>
+					<Button onClick={updateUserImage} disabled={!newImage?.url}>Accept</Button>
+				</div>
+			) : (
+				<Button
+					isIconOnly
+					color='warning'
+					variant='bordered'
+					title='Edit Photo'
+					onClick={handleChangePhoto}>
+					<EditIcon />
+				</Button>
+			)}
+		</CardHeader>
+	);
 };
 
-export const UserProfileBody = ({ user, openCountry, stringCountry, newCountry, selectCountry, handleChangeCountry, updateUserCountry, openCity, stringCity, newCity, selectCity, handleChangeCity, updateUserCity, deportes, selectSport, newSport, updateUserSport, getNewExp, newExp, updateUserExp, getNewAge, newAge, updateUserAge }) => (
+export const UserProfileBody = ({
+	user,
+	openCountry,
+	stringCountry,
+	newCountry,
+	selectCountry,
+	handleChangeCountry,
+	updateUserCountry,
+	openCity,
+	stringCity,
+	newCity,
+	selectCity,
+	handleChangeCity,
+	updateUserCity,
+	deportes,
+	selectSport,
+	newSport,
+	updateUserSport,
+	getNewExp,
+	newExp,
+	updateUserExp,
+	getNewAge,
+	newAge,
+	updateUserAge,
+}) => (
+	<CardBody className='elBody'>
+		<h2>Email: {user?.email}</h2>
+		{user?.role === 0 ? (
+			<h2>Plan: Free Plan</h2>
+		) : user?.role === 1 ? (
+			<h2>Plan: Pay Plan</h2>
+		) : (
+			<h2>Plan: Admin </h2>
+		)}
+		<CountrySelect
+			stringCountry={stringCountry}
+			openCountry={openCountry}
+			newCountry={newCountry}
+			selectCountry={selectCountry}
+			handleChangeCountry={handleChangeCountry}
+			updateUserCountry={updateUserCountry}
+		/>
+		<CitySelect
+			openCountry={openCountry}
+			handleChangeCountry={handleChangeCountry}
+			updateUserCountry={updateUserCountry}
+			openCity={openCity}
+			stringCity={stringCity}
+			newCountry={newCountry}
+			newCity={newCity}
+			selectCity={selectCity}
+			handleChangeCity={handleChangeCity}
+			updateUserCity={updateUserCity}
+		/>
 
-    <CardBody className='elBody'>
-      <h2>Email: {user?.email}</h2>
-      {user?.role === 0 ? (
-        <h2>Plan: Free Plan</h2>
-      ) : user?.role === 1 ? (
-        <h2>Plan: Pay Plan</h2>
-      ) : (
-        <h2>Plan: Admin </h2>
-      )}
-      <CountrySelect stringCountry = {stringCountry} openCountry = {openCountry} newCountry={newCountry} selectCountry={selectCountry} handleChangeCountry={handleChangeCountry} updateUserCountry={updateUserCountry} />
-      <CitySelect openCountry={openCountry} handleChangeCountry={handleChangeCountry} updateUserCountry={updateUserCountry} openCity = {openCity} stringCity = {stringCity} newCountry={newCountry} newCity={newCity} selectCity={selectCity} handleChangeCity={handleChangeCity} updateUserCity={updateUserCity} />
+		{!user?.sport ? (
+			<h2>
+				Deporte:
+				<select value={newSport} onChange={selectSport}>
+					{deportes.map(deporte => (
+						<option> {deporte}</option>
+					))}
+				</select>
+				<Button
+					color='warning'
+					variant='bordered'
+					size='sm'
+					onClick={updateUserSport}>
+					Accept
+				</Button>
+			</h2>
+		) : (
+			<h2> Deporte: {user?.sport?.name}</h2>
+		)}
 
-      {!user?.sport ? <h2>Deporte: <select value={newSport} onChange={selectSport}>
-        {deportes.map((deporte) => (
-          <option> {deporte}</option>
-        ))}
-      </select> <Button color="warning" variant="bordered" size="sm" onClick={updateUserSport} > Accept </Button> </h2>
-      :<h2> Deporte: {user?.sport?.name}</h2>}
+		{!user?.age ? (
+			<h2>
+				Edad: <InputAge getNewAge={getNewAge} newAge={newAge} />{' '}
+				<Button
+					color='warning'
+					variant='bordered'
+					size='sm'
+					onClick={updateUserAge}>
+					Accept
+				</Button>
+			</h2>
+		) : (
+			<h2> Edad: {user?.age} </h2>
+		)}
 
-      {!user?.age ? <h2>Edad:  <InputAge getNewAge={getNewAge} newAge={newAge}  /> <Button color="warning" variant="bordered" size="sm" onClick={updateUserAge} > Accept </Button> </h2>
-      : <h2> Edad: {user?.age} </h2>
-      }
-
-      {!user?.expYearsSports ? <h2>Años de experiencia: <InputExp getNewExp={getNewExp} newExp={newExp}  /> <Button color="warning" variant="bordered" size="sm" onClick={updateUserExp} > Accept </Button> </h2>
-      : <h2> Años de experiencia: {user?.expYearsSports}  </h2> 
-      }
-      
-      
-    </CardBody>
-  );
+		{!user?.expYearsSports ? (
+			<h2>
+				Años de experiencia: <InputExp getNewExp={getNewExp} newExp={newExp} />{' '}
+				<Button
+					color='warning'
+					variant='bordered'
+					size='sm'
+					onClick={updateUserExp}>
+					Accept
+				</Button>
+			</h2>
+		) : (
+			<h2> Años de experiencia: {user?.expYearsSports} </h2>
+		)}
+	</CardBody>
+);
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,166 +1,207 @@
 'use client';
-import { useCountryCity } from '@/zustand/store/countryStore';
 import { useNationAndCity } from '@/zustand/store/countryAndCityID';
 import './styles.scss';
 import { useEffect, useState } from 'react';
-import {Card, CardHeader, CardBody, Image, CircularProgress, Button } from '@nextui-org/react';
+import { Card, CircularProgress } from '@nextui-org/react';
 import { useSession } from 'next-auth/react';
 import { useUserDetail } from '@/zustand/store/userDetail';
 import { UserProfileBody, UserProfileHeader } from './HeadBodyProfile';
 import { useUserProfile } from '@/zustand/store/userProfile';
 import { getCookie } from 'cookies-next';
 import { deportes } from '@/utils/dataRegister';
-
+import { toastError, toastInfo } from '@/helpers/toast';
 
 const UserProfile = () => {
 	const { data: session } = useSession();
-	const theUser = session?.token?.user;
-	const cookie = getCookie("jsdklfsdjklfdsjfds")
-	const {getProfile, user} = useUserProfile()
-	const {updateUserRole} = useUserDetail()
-	const {getCityId, getCountryId, stringCity, stringCountry} = useNationAndCity()
-	let [openImage, setOpenImage] = useState(false)
-	let [openCountry, setOpenCountry] = useState(false)
-	let [openCity, setOpenCity] = useState(false)
-	let [openName, setOpenName] = useState(false)
-	let [newName, setNewName] = useState("")
-	let [newCountry, setNewCountry] = useState("")
-	let [newCity, setNewCity] = useState("")
-	let [newImage, setNewImage] = useState(null)
-	let [newSport, setNewSport] = useState("")
-	let [newExp, setNewExp] = useState("")
-	let [newAge, setNewAge] = useState("")
-	
-	useEffect(() => {
-		getProfile(cookie)
-		setNewName(user?.username)
-		setNewCountry(user?.nation?.name)
-		setNewCity(user?.city?.name)
-		if(!user.sport){
-			console.log("no deporte")
-		}
-		}, [user?.username, user?.id ] )
+	const cookie = getCookie('jsdklfsdjklfdsjfds');
+	const { getProfile, user } = useUserProfile();
+	const { updateUserRole } = useUserDetail();
+	const { stringCity, stringCountry } = useNationAndCity();
+	let [openImage, setOpenImage] = useState(false);
+	let [openCountry, setOpenCountry] = useState(false);
+	let [openCity, setOpenCity] = useState(false);
+	let [openName, setOpenName] = useState(false);
+	let [newName, setNewName] = useState('');
+	let [newCountry, setNewCountry] = useState('');
+	let [newCity, setNewCity] = useState('');
+	let [newImage, setNewImage] = useState(null);
+	let [newSport, setNewSport] = useState('');
+	let [newExp, setNewExp] = useState('');
+	let [newAge, setNewAge] = useState('');
+	const [updated, setUpdated] = useState(false);
 
+	useEffect(() => {
+		getProfile(cookie);
+		setNewName(user?.username);
+		setNewCountry(user?.nation?.name);
+		setNewCity(user?.city?.name);
+		if (updated) {
+			getProfile(cookie);
+			setUpdated(false);
+		}
+		if (!user.sport) {
+			console.log('no deporte');
+		}
+	}, [user?.username, user?.id, updated]);
 	const handleChangePhoto = () => {
-		setOpenImage(!openImage)
-		setOpenCity(false)
-		setOpenName(false)
-		setOpenCountry(false)
-	}
+		setOpenImage(!openImage);
+		setOpenCity(false);
+		setOpenName(false);
+		setOpenCountry(false);
+	};
 	const handleChangeCountry = () => {
-		setOpenCountry(!openCountry)
-		setOpenCity(false)
-		setOpenName(false)
-		setOpenImage(false)
-		setNewCountry(stringCountry)
-	}
+		setOpenCountry(!openCountry);
+		setOpenCity(false);
+		setOpenName(false);
+		setOpenImage(false);
+		setNewCountry(stringCountry);
+	};
 	const handleChangeCity = () => {
-		setOpenCity(!openCity)
-		setOpenCountry(false)
-		setOpenName(false)
-		setOpenImage(false)
-	}
+		setOpenCity(!openCity);
+		setOpenCountry(false);
+		setOpenName(false);
+		setOpenImage(false);
+	};
 	const handleChangeName = () => {
-		setOpenName(!openName)
-		setOpenCity(false)
-		setOpenCountry(false)
-		setOpenImage(false)
-	} 
-	const getNewName = (event) => {
-		setNewName(event.target.value)
-	}
-	const getNewExp = (event) => {
-		setNewExp(event.target.value)
-	}
-	const getNewAge = (event) => {
-		setNewAge(event.target.value)
-	}
-	const getNewImage = (e) => {
+		setOpenName(!openName);
+		setOpenCity(false);
+		setOpenCountry(false);
+		setOpenImage(false);
+	};
+	const getNewName = event => {
+		setNewName(event.target.value);
+	};
+	const getNewExp = event => {
+		setNewExp(event.target.value);
+	};
+	const getNewAge = event => {
+		setNewAge(event.target.value);
+	};
+	const getNewImage = e => {
 		const { info } = e;
 		const { url, public_id } = info;
-		setNewImage({ url, id:public_id });
-	}
+		setNewImage({ url, id: public_id });
+	};
 	const updateUserName = () => {
-		const update = {id: user?._id, username: newName}
-		console.log(update)
-		updateUserRole(update)
-		setOpenName(false)
-		getProfile(cookie)
-		window.location.reload()
-	}
+		const update = { id: user?._id, username: newName };
+		updateUserRole(update);
+		setOpenName(false);
+		getProfile(cookie);
+		setUpdated(true);
+	};
 	const updateUserAge = () => {
-		const update = {id: user?._id, age: newAge}
-		updateUserRole(update)
-		getProfile(cookie)
-		console.log(update)
-		// window.location.reload()
-	}
+		const update = { id: user?._id, age: newAge };
+		updateUserRole(update);
+		getProfile(cookie);
+	};
 	const updateUserExp = () => {
-		const update = {id: user?._id, expYearsSports: newExp}
-		updateUserRole(update)
-		getProfile(cookie)
-		// window.location.reload()
-	}
+		const update = { id: user?._id, expYearsSports: newExp };
+		updateUserRole(update);
+		getProfile(cookie);
+	};
 	const updateUserCountry = () => {
-		const update = {id: user?._id, nation: newCountry, city:newCity}
-		updateUserRole(update)
-		setOpenCountry(false)
-		setOpenCity(false)
-		getProfile(cookie)
-		// window.location.reload()
-	}
-	const updateUserCity = () => {	
-		const update = {id: user?._id, city: newCity}
-		updateUserRole(update)
-		setOpenCity(false)
-		getProfile(cookie)
-		// window.location.reload()
-	}
-	const updateUserSport = () => {	
-		const update = {id: user?._id, sport: newSport}
-		updateUserRole(update)
-		getProfile(cookie)
-		// window.location.reload()
-	}
-	
-	const updateUserImage = () => {
-		const update = {id: user?._id, imagen: {
-			public_id: newImage.id,
-			secure_url: newImage.url
-		}}
-		updateUserRole(update)
-		console.log (update)
+		const update = { id: user?._id, nation: newCountry, city: newCity };
+		updateUserRole(update);
+		setOpenCountry(false);
+		setOpenCity(false);
+		getProfile(cookie);
+	};
+	const updateUserCity = () => {
+		const update = { id: user?._id, city: newCity };
+		updateUserRole(update);
+		setOpenCity(false);
+		getProfile(cookie);
+	};
+	const updateUserSport = () => {
+		const update = { id: user?._id, sport: newSport };
+		updateUserRole(update);
+		getProfile(cookie);
+	};
 
-	}
-	const selectCountry = (val) => {
+	const updateUserImage = async () => {
+		try {
+			const update = {
+				id: user?._id,
+				imagen: {
+					public_id: newImage.id,
+					secure_url: newImage.url,
+				},
+			};
+			await updateUserRole(update);
+			setUpdated(true);
+			toastInfo('A actualizado la foto de perfil');
+			setNewImage(null);
+		} catch (error) {
+			toastError(
+				'No se pudo cambiar la foto de perfil, contacta a un moderador si el problema persiste'
+			);
+		}
+	};
+	const selectCountry = val => {
 		setNewCountry(val);
 		setNewCity('');
-	}
-	const selectCity = (val) => {
+	};
+	const selectCity = val => {
 		setNewCity(val);
-	}
-	const selectSport = (event) => {
-		setNewSport(event.target.value)
-	}
-
+	};
+	const selectSport = event => {
+		setNewSport(event.target.value);
+	};
 	return (
 		<article>
-		  {user && user?._id && Object.keys(user).length > 0 ? (
-			<Card className='main'>
-			  <UserProfileHeader newImage={newImage} updateUserImage={updateUserImage} getNewImage={getNewImage} openImage={openImage} user={user} openName={openName} handleChangeName={handleChangeName} handleChangePhoto={handleChangePhoto} updateUserName={updateUserName} getNewName = {getNewName} newName = {newName} session = {session}/>
-			  <UserProfileBody updateUserAge={updateUserAge} updateUserExp={updateUserExp} getNewAge={getNewAge} newAge={newAge} getNewExp={getNewExp} newExp={newExp} updateUserSport={updateUserSport} newSport={newSport} selectSport={selectSport} deportes={deportes} user={user} openCountry={openCountry} stringCountry={stringCountry} newCountry={newCountry} selectCountry={selectCountry} handleChangeCountry={handleChangeCountry} updateUserCountry={updateUserCountry} openCity={openCity} stringCity={stringCity} newCity={newCity} selectCity={selectCity} handleChangeCity={handleChangeCity} updateUserCity={updateUserCity} />
-			</Card>
-		  ) : (
-			<div className='centered'>
-			  <CircularProgress
-				className='loading'
-				label='Loading...'
-				color='warning'
-			  />
-			</div>
-		  )}
+			{user && user?._id && Object.keys(user).length > 0 ? (
+				<Card className='main'>
+					<UserProfileHeader
+						newImage={newImage}
+						updateUserImage={updateUserImage}
+						getNewImage={getNewImage}
+						openImage={openImage}
+						user={user}
+						openName={openName}
+						handleChangeName={handleChangeName}
+						handleChangePhoto={handleChangePhoto}
+						updateUserName={updateUserName}
+						getNewName={getNewName}
+						newName={newName}
+						session={session}
+					/>
+					<UserProfileBody
+						updateUserAge={updateUserAge}
+						updateUserExp={updateUserExp}
+						getNewAge={getNewAge}
+						newAge={newAge}
+						getNewExp={getNewExp}
+						newExp={newExp}
+						updateUserSport={updateUserSport}
+						newSport={newSport}
+						selectSport={selectSport}
+						deportes={deportes}
+						user={user}
+						openCountry={openCountry}
+						stringCountry={stringCountry}
+						newCountry={newCountry}
+						selectCountry={selectCountry}
+						handleChangeCountry={handleChangeCountry}
+						updateUserCountry={updateUserCountry}
+						openCity={openCity}
+						stringCity={stringCity}
+						newCity={newCity}
+						selectCity={selectCity}
+						handleChangeCity={handleChangeCity}
+						updateUserCity={updateUserCity}
+					/>
+				</Card>
+			) : (
+				<div className='centered'>
+					<CircularProgress
+						className='loading'
+						label='Loading...'
+						color='warning'
+					/>
+				</div>
+			)}
 		</article>
-	  );
+	);
 };
 
 export default UserProfile;
