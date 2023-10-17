4791cdf2ce035b8a400513cf8418e278fe894815
NONHIGH <angelones_24@hotmail.com>
2023-10-17 13:00:07 -0300

    metrics styles

--- src/components/metrics/Bar.jsx
+++ src/components/metrics/Bar.jsx
@@ -12,9 +12,42 @@ import randomColor from 'randomcolor';
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 const BarChart = ({ data, options }) => {
+	ChartJS.defaults.font.size = 25;
 	const backgroundColors = randomColor({ count: data?.labels?.length });
 	data.datasets[0].backgroundColor = backgroundColors;
-	return <Bar data={data} options={options} />;
+	options = {
+		...options,
+		scales: {
+			x: {
+				barThickness: 10,
+				grid: {
+					offset: true,
+				},
+			},
+		},
+		indexAxis: 'y',
+		plugins: {
+			title: {
+				display: true,
+				text: "Por país",
+				fontSize: 18,
+			},
+			legend: {
+                labels: {
+                   font:{
+					size: 30,
+				   }
+                }
+			},
+			labels:{
+				font:{
+					size:30,
+				}
+			}
+		},
+	};
+	data.datasets[0].barThickness = 10;
+	return <Bar data={data} options={options} style={{background:'#ffffffe5'}} />;
 };
 
 export default BarChart;
