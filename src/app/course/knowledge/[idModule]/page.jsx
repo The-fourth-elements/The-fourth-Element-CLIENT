d862a517ad08e9ab804e4acba4fdd9cd74f81f1c
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-10-29 22:58:17 -0300

    estilos autorregistro view

--- src/app/course/knowledge/[idModule]/page.jsx
+++ src/app/course/knowledge/[idModule]/page.jsx
@@ -7,28 +7,25 @@ import {
 	Card,
 	CardBody,
 	CardHeader,
+	CardFooter,
 	CircularProgress,
 	useDisclosure,
 } from '@nextui-org/react';
 import { useSelectedModule } from '@/zustand/store/selectedModule';
 
-
 function page({ params }) {
 	const { idModule } = params;
-	const [isLoading, setIsLoading] = useState(true)	
-	const {module, getModule} = useSelectedModule()
-
+	const [isLoading, setIsLoading] = useState(true);
+	const { module, getModule } = useSelectedModule();
 
 	useEffect(() => {
 		getModule(idModule).then(() => {
 			setIsLoading(false);
-		  });
-
-	}, [])
-	
+		});
+	}, []);
 
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
-	
+
 	if (isLoading) {
 		return (
 			<>
@@ -38,28 +35,34 @@ function page({ params }) {
 	}
 
 	return (
-		<>
-			<Card className='w-full'>
+		<div className='flex flex-col items-center mt-8 rounded-lg'>
+			<Card className='w-full  sm:w-[85vw] md:w-[75vw] rounded-lg'>
 				<CardHeader>
-					<h3 className='w-full text-center'>Titulo para el usuario</h3>
+					<h3 className='w-fit mx-auto p-2 text-center '>
+						Titulo para el usuario
+					</h3>
 				</CardHeader>
 				<CardBody className=''>
-					<p className='bg-white text-black w-[50%]'>
-						Esta es una charla introductoria a lo que vera el usuario cuando
-						este viendo este autoconocimiento sin saber que es, puede
-						explicarle que es lo que vera y que tendra que hacer para poder
-						tener un mejor entendimiento del tema.
+					<p className=' md:w-[60%] sm:w-[90%] w-[100%] mx-auto p-5  bg-primary rounded-lg'>
+						Esta es una charla introductoria a lo que verá el usuario cuando
+						está viendo este autoconocimiento sin saber qué es. Puede explicarle
+						qué es lo que vera y qué tendra que hacer para poder tener un mejor
+						entendimiento del tema.
 					</p>
 				</CardBody>
+				<CardFooter>
+					<Button onClick={onOpen} className='mx-auto w-fit px-5'>
+						Autorregistro
+					</Button>
+					<RenderSelfKnowledge
+						data={module?.selfKnowledge}
+						isOpen={isOpen}
+						onOpenChange={onOpenChange}
+						onOpen={onOpen}
+					/>
+				</CardFooter>
 			</Card>
-			<Button onClick={onOpen}>abrir modal prueba</Button>
-			<RenderSelfKnowledge
-				data={module?.selfKnowledge}
-				isOpen={isOpen}
-				onOpenChange={onOpenChange}
-				onOpen={onOpen}
-			/>
-		</>
+		</div>
 	);
 }
 
