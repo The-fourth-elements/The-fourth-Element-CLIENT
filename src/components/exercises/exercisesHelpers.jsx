f6cb933fa776bfc53c759094700b33a1e1c6ef9e
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-11-13 17:38:01 -0300

    ejercicios se pueden responder bien

--- src/components/exercises/exercisesHelpers.jsx
+++ src/components/exercises/exercisesHelpers.jsx
@@ -3,6 +3,8 @@ import { toastSuccess, toastError } from '@/helpers/toast';
 import { Accordion, AccordionItem, Button } from '@nextui-org/react';
 import { Formik, Form } from 'formik';
 import { useState } from 'react';
+import { getCookie } from 'cookies-next';
+
 
 export const renderExercises = (exerciseIndex, elem, handleExerciseClick) => {
 	return (
@@ -13,7 +15,7 @@ export const renderExercises = (exerciseIndex, elem, handleExerciseClick) => {
 			<div className='flex justify-between items-center'>
 				<Button
 					className='cursor-pointer rounded-full'
-					onPress={() => handleExerciseClick(elem?.questions)}>
+					onPress={() => handleExerciseClick(elem)}>
 					Entrar
 				</Button>
 			</div>
@@ -21,12 +23,27 @@ export const renderExercises = (exerciseIndex, elem, handleExerciseClick) => {
 	);
 };
 
-const handleSubmit = async values => {
+const handleSubmit = async (values, exerciseId) => {
 	try {
-	  const answersArray = Object.keys(values).map(key => values[key]);
-	  console.log('answersArray ', answersArray);
+	  const userId = getCookie('jsdklfsdjklfdsjfds');
   
-	  toastSuccess('Respuestas enviadas con éxito');
+	  const answersArray = Object.keys(values).map(key => values[key]);
+  
+	  const response = await fetch(`${process.env.API_BACKEND}responseEx/${exerciseId}/user/${userId}`, {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ responses: answersArray }),
+	  });
+  
+	  if (response.ok) {
+		const responseData = await response.json();
+		toastSuccess(responseData.message);
+	  } else {
+		const errorData = await response.json();
+		toastError(`Error: ${errorData.message}`);
+	  }
 	} catch (error) {
 	  console.log(error.message);
 	  toastError('Error ', error.message);
@@ -34,17 +51,18 @@ const handleSubmit = async values => {
   };
   
 
-export const renderTextSection = questions => {
-	return questions ? (
+export const renderTextSection = (exercise) => {
+	
+	return exercise?.questions ? (
 		<Formik
-			initialValues={questions.reduce((acc, _, index) => {
+			initialValues={exercise?.questions.reduce((acc, _, index) => {
 				acc[`answer_${index}`] = '';
 				return acc;
 			}, {})}
-			onSubmit={values => handleSubmit(values)}>
+			onSubmit={values => handleSubmit(values, exercise._id)}>
 			<Form className='flex flex-col'>
 				<Accordion defaultExpandedKeys={['0']}>
-					{questions.map((elem, index) => (
+					{exercise?.questions.map((elem, index) => (
 						<AccordionItem
 							key={index}
 							textValue={elem}
@@ -58,14 +76,14 @@ export const renderTextSection = questions => {
 								labelPlacement='inside'
 								placeholder='Escribe tu respuesta aquí'
 							/>
-							<Button
-								className='w-fit px-3 py-1 m-0 mb-5 mx-auto bg-green-600 hover-bg-green-800'
-								type='submit'>
-								Guardar respuesta
-							</Button>
 						</AccordionItem>
 					))}
 				</Accordion>
+				<Button
+					className='w-fit px-3 py-1 m-0 mb-5 mx-auto bg-green-600 hover-bg-green-800'
+					type='submit'>
+					Guardar respuestas
+				</Button>
 			</Form>
 		</Formik>
 	) : (
