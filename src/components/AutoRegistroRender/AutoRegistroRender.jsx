3abd30bc87ff8751d3f5ad578e99b7e1d4a8bd99
Kyrua <ang02aezs@gmail.com>
2023-11-14 02:27:03 -0600

    responsesSR

--- src/components/AutoRegistroRender/AutoRegistroRender.jsx
+++ src/components/AutoRegistroRender/AutoRegistroRender.jsx
@@ -1,63 +1,90 @@
 'use client'
 import './AutoRegistroRender.scss'
 import { useAutoRegistro } from '@/zustand/store/autoRegistroStore'
-import { useEffect, useState } from 'react'
 import { CircularProgress } from '@nextui-org/react'
-import SliderAutoRegistro from '@/helpers/SliderAutoRegistro'
+// import SliderAutoRegistro from '@/helpers/SliderAutoRegistro'
+import { useState, useEffect } from 'react'
+import { Slider } from '@nextui-org/react'
+import { getCookie } from 'cookies-next'
 
 // const RenderAutoRegistro = ({id}) => {
 const RenderAutoRegistro = () => {
     const id = '6545fe59cb4b95c0832e6e1f'
-    const { getAutoRegistro, excersice } = useAutoRegistro()
-    const [responses, setResponses] = useState(excersice?.questions?.map((question) => ""))
+    const { getAutoRegistro, excersice, createResponseSR, addResponseSRToUser} = useAutoRegistro()
+    const userId = getCookie('jsdklfsdjklfdsjfds');
+    const [userResponses, setUserResponses] = useState([]);
+    const [value, setValue] = useState()
     useEffect(() => {
-        getAutoRegistro(id)
-    }, [])
+        if(Object.keys(excersice).length === 0){
+            const data = getAutoRegistro(id); // Llama a getAutoRegistro de manera síncrona
+        }
+        if (excersice.questions) {
+            // console.log("holas")
+            setUserResponses(excersice.questions.map(() => 3));
+        }
+    }, [excersice]);
 
     const handleButton = () => {
-        console.log(excersice)
-        console.log(responses)
+        console.log(excersice);
+        console.log(userResponses);
+        console.log(userId)
     }
 
     const handleChangeResponse = (event, index) => {
-        console.log(event)
-        console.log(index)
-        const newResponses = [...responses]
-        newResponses[index] = event
+        const newResponses = [...userResponses];
+        newResponses[index] = event;
+        setUserResponses(newResponses); // Actualiza el estado de las respuestas del usuario
+        console.log(newResponses)
     }
-    return(
-        Object.keys(excersice).length > 0 ?(<main className="mainAutoRender">
-            <header>
-                <h1>{excersice?.name}</h1>
-                <h2>{excersice?.type}</h2>
-            </header>
-            <article>
-                {excersice.questions.map((question, index) => (
-                    <section className="section1AutoRender">
-                        <h3>{question?.selfQuestion}</h3>
-                        <h3>{question?.agree}</h3>
-                        <SliderAutoRegistro index = {index} responses={responses} handleChangeResponse = {handleChangeResponse}/>
-                        <h3>{question?.disagree}</h3>
-
-                    </section>
-                ))}
-                
-            </article>
-            <footer>
-                <button className="button1FooterAutoRender">
-
-                </button>
-                <button onClick={handleButton} className="button2FooterAutoRender">
-                    clickeame
-                </button>
-            </footer>
-        </main>)
-        : <CircularProgress
-        className='loading'
-        label='Loading...'
-        color='warning'
-    />
-    )
+    return (
+        Object.keys(excersice).length > 0 ? (
+            <main className="mainAutoRender">
+                <header>
+                    <h1>{excersice?.name}</h1>
+                    <h2>{excersice?.type}</h2>
+                </header>
+                <article>
+                    {excersice?.questions?.map((question, index) => (
+                        <section className="section1AutoRender" key={index}>
+                            <h3 className='autoRegistroQuestion'>{question?.selfQuestion}</h3>
+                            <h3 className='autoRegistroAgree'>{question?.agree}</h3>
+                            <Slider className='autoRegistroSlider'
+        onChange={(event) => handleChangeResponse(event, index)}
+        color={userResponses[index] === 3 ? "foreground" : userResponses[index] < 3 ? "danger" : "success"}
+        size="md"
+        step={1}
+        defaultValue={3}
+        showSteps={true} 
+        maxValue={5} 
+        minValue={1}
+        marks={[
+            {value : 1,
+            label : 1},
+            {value : 2,
+            label : 2},
+            {value : 3,
+            label : 3},
+            {value : 4,
+            label : 4},
+            {value : 5,
+            label : 5}
+        ]}
+        />
+                            <h3 className='autoRegistroDisagree'>{question?.disagree}</h3>
+                        </section>
+                    ))}
+                </article>
+                <footer>
+                    <button className="button1FooterAutoRender"></button>
+                    <button onClick={handleButton} className="button2FooterAutoRender">
+                        clickeame
+                    </button>
+                </footer>
+            </main>
+        ) : (
+            <CircularProgress className='loading' label='Loading...' color='warning' />
+        )
+    );
 }
 
 export default RenderAutoRegistro;
--- src/zustand/actions/autoRegistroActions.js
+++ src/zustand/actions/autoRegistroActions.js
@@ -31,3 +31,41 @@ export const getAutoRegistro = async (id) => {
         toastError(error);
     }
 }
+
+export const createResponseSR = async (body) => {
+    try {
+        const response = await fetch(`${process.env.API_BACKEND}responseSR/${body.selfRegisterId}/user/${body.userID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        })
+        const data = await response.json()
+        if (data.error) {
+            throw data.error;
+        }
+        return data
+    } catch (error) {
+        toastError(error);
+    }
+}
+
+export const addResponseSRToUser = async (body) => {
+    try {
+        const response = await fetch(`${process.env.API_BACKEND}responseSR/${body.responseSRId}/user/${body.userID}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        })
+        const data = await response.json()
+        if (data.error) {
+            throw data.error;
+        }
+        return data
+    } catch (error) {
+        toastError(error);
+    }
+}
