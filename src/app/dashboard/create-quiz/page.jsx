6eadf73a1964af6141f30f91337ce5862cbf8ff7
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-11-16 19:18:09 -0300

    arreglo para el deploy

--- src/app/dashboard/create-quiz/page.jsx
+++ src/app/dashboard/create-quiz/page.jsx
@@ -1,11 +1,9 @@
 import QuizForm from "@/components/QuizForm/QuizForm";
-import RenderAutoRegistro from "@/components/AutoRegistroRender/AutoRegistroRender";
 
 const CreateQuiz = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
         <QuizForm update={false}/>
-        {/* <RenderAutoRegistro/> */}
         </div>
     )
 }
