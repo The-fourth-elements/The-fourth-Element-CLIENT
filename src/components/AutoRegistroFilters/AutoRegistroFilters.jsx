1f31380a20b323f7da5ab9eb193f708fb87ae868
Kyrua <ang02aezs@gmail.com>
2023-11-21 16:28:36 -0600

    filtros sin graficas

--- src/components/AutoRegistroFilters/AutoRegistroFilters.jsx
+++ src/components/AutoRegistroFilters/AutoRegistroFilters.jsx
@@ -1,36 +1,16 @@
 'use client'
 import { useState } from "react"
+import { handleSelectFilterAuto } from "@/helpers/handleSelectAutoFilters"
 
 const AutoRegistroFilters = (responses) => {
-    console.log("responses:", responses)
+    // console.log("responses:", responses)
     const [valueSelect, setValueSelect] = useState("todos")
     // const [fecha, setFecha] = useState(null)
-    let responsesToShow;
-    const handleSelectFilterAuto = (event) => {
-        setValueSelect(event?.target?.value)
-        if(valueSelect === "ultimosDiez") {
-            responsesToShow = responses?.responses?.slice(-10)
-        }
-        if(event?.target?.value === "ultimaSemana"){
-            console.log(responses?.responses)
-            const fecha = new Date (Date.now())
-            const year = fecha.getFullYear()
-            console.log(year)
-            const yearMatched = responses?.responses?.filter((response) => 
-                parseInt(response?.date) === 20
-            )
-            console.log("todos: ", yearMatched)
-        }
-        if(valueSelect === "ultimoMes") {
+    let [responsesToShow, setResponsesToShow] = useState([])
     
-        }
-        if(valueSelect === "todos"){
-    
-        }
-    }
     return(
         <section>
-            <select name="" id="" onChange={handleSelectFilterAuto}>
+            <select name="" id="" onChange={(event) => handleSelectFilterAuto(event, responses, setResponsesToShow, setValueSelect)}>
                 <option value="ultimosDiez">ultimos 10 auto registros</option>
                 <option value="ultimaSemana">autoregistros de la ultima semana</option>
                 <option value="ultimoMes">autoregistros del ultimo mes</option>
@@ -42,6 +22,8 @@ const AutoRegistroFilters = (responses) => {
                 {valueSelect}
                 {/* {fecha} */}
             </div>
+
+            <button  onClick={() => {console.log(responsesToShow)}}>showREsponses</button>
         </section>
     )
 }
