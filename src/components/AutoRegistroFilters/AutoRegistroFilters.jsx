835d5de66b0abfe713ff19d5b750d30a7c0aa93a
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-11-23 23:13:12 -0300

    autoregistro funcional con Bar Chart

--- src/components/AutoRegistroFilters/AutoRegistroFilters.jsx
+++ src/components/AutoRegistroFilters/AutoRegistroFilters.jsx
@@ -1,80 +1,100 @@
-'use client'
-import "./AutoRegistroFilters.scss"
-import { useState } from "react"
-import { handleSelectFilterAuto } from "@/helpers/handleSelectAutoFilters"
-import { Select, SelectItem } from "@nextui-org/react"
-import UserRecords from "@/features/user/knowledge/UserRecords"
+'use client';
+import './AutoRegistroFilters.scss';
+import { useState } from 'react';
+import { handleSelectFilterAuto } from '@/helpers/handleSelectAutoFilters';
+import { Select, SelectItem } from '@nextui-org/react';
+import UserRecords from '@/features/user/knowledge/UserRecords';
 
-const AutoRegistroFilters = (responses) => {
-    // console.log("responses:", responses)
-    // const responses = []
-    const [valueSelect, setValueSelect] = useState("todos")
-    // const [fecha, setFecha] = useState(null)
-    let [responsesToShow, setResponsesToShow] = useState(responses?.responses)
-    let [responsesToCheck, setResponsesToCheck] =useState(responses?.responses)
-    const datasets = responsesToShow?.map((response) => {
-		const fecha = new Date (response?.date)
+const AutoRegistroFilters = responses => {
+
+	const [valueSelect, setValueSelect] = useState('todos');
+	let [responsesToShow, setResponsesToShow] = useState(responses?.responses);
+	let [responsesToCheck, setResponsesToCheck] = useState(responses?.responses);
+
+	const datasets = responsesToShow?.map(response => {
+		const fecha = new Date(response?.date);
 		const dia = fecha.getDate();
 		const mes = fecha.getMonth() + 1;
 		return {
-                label: "",
-                data: response?.response,
-                fill: false,
-                backgroundColor: 'none',
-                borderColor: 'rgb(255, 99, 132)',
-                pointBackgroundColor: 'rgb(255, 99, 132)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: 'rgb(255, 99, 132)'
-    };
-})
+			label: '',
+			data: response?.response,
+			fill: false,
+			backgroundColor: 'none',
+			borderColor: 'rgb(255, 99, 132)',
+			pointBackgroundColor: 'rgb(255, 99, 132)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgb(255, 99, 132)',
+		};
+	});
 
-        const handleToCheckAutoRegistro = (id) => {
-            const responsesFiltered = responses?.responses?.filter((newResponse) => newResponse._id === id)
-            setResponsesToShow(responsesFiltered)
-            setValueSelect("")
-        }
-    return(
-        responsesToShow?.length > 0 ? (<section className="autoRegistroFilters">
-            <header>
-                <h2>Conoce tus autoregistros</h2>
-            </header>
-            <div className="divForSelect">
-                <label htmlFor="select">Filtros</label>
-            <Select value={valueSelect} className="selectAutoRegistros" label="Filtrar Autoregistros" onChange={(event) => handleSelectFilterAuto(event, responses, setResponsesToShow, setValueSelect)}>
-                <SelectItem key="ultimosDiez" value="ultimosDiez">ultimos 10 auto registros</SelectItem>
-                <SelectItem key="ultimaSemana" value="ultimaSemana">autoregistros de la ultima semana</SelectItem>
-                <SelectItem key="ultimoMes" value="ultimoMes">autoregistros del ultimo mes</SelectItem>
-                <SelectItem key="todos" value="todos">todos los autoregistros</SelectItem>
-            </Select>
-            </div>
-             <div className="divForGraphic">
-                <UserRecords datasets = {datasets}/>
-                {/* <button  onClick={() => {console.log(datasets)}}>showREsponses</button> */}
-                
-            </div>
-            <div className="divForShowAutoRegistros">
-                    {   responsesToCheck?.map((response, index) => {
-                            const fecha = new Date (response?.date)
-                            const dia = fecha.getDate();
-                            const mes = fecha.getMonth() + 1;
-                            return (
-                            <h3 onClick={() => handleToCheckAutoRegistro(response?._id)}>
-                                {`Registro dia ${dia}/${mes}`}
-                            </h3>)
-                            }
-                        )
-                    }
-            </div>
-            </section>)
-            :
-            (
-                <section className="sectionNoResponses">
-                    <h2>Conoce tus autoregistros</h2>
-                    <h3>Parece que aun no has hecho ningun autoregistro</h3>
-                </section>
-            )
-    )
-}
+	const handleToCheckAutoRegistro = id => {
+		const responsesFiltered = responses?.responses?.filter(
+			newResponse => newResponse._id === id
+		);
+		setResponsesToShow(responsesFiltered);
+		setValueSelect('');
+	};
+	return responsesToShow?.length > 0 ? (
+		<section className='autoRegistroFilters'>
+			<header>
+				<h2>Conoce tus autoregistros</h2>
+			</header>
+			<div className='divForSelect'>
+				<label htmlFor='select'>Filtrar Autoregistros</label>
+				<Select
+					value={valueSelect}
+					className='selectAutoRegistros'
+                    aria-label='Seleccione un filtro'
+                    placeholder='Seleccione un filtro'
+					
+					onChange={event =>
+						handleSelectFilterAuto(
+							event,
+							responses,
+							setResponsesToShow,
+							setValueSelect
+						)
+					}>
+					<SelectItem key='ultimosDiez' value='ultimosDiez'>
+						Últimos 10 auto registros
+					</SelectItem>
+					<SelectItem key='ultimaSemana' value='ultimaSemana'>
+						Autoregistros de la última semana
+					</SelectItem>
+					<SelectItem key='ultimoMes' value='ultimoMes'>
+                        Autoregistros del ultimo mes
+					</SelectItem>
+					<SelectItem key='todos' value='todos'>
+						Todos los Autoregistros
+					</SelectItem>
+				</Select>
+			</div>
+            
+			<div className='divForGraphic'>
+				<UserRecords
+					data={responsesToShow}
+				/> 
+			</div>
+			<div className='divForShowAutoRegistros'>
+				{responsesToCheck?.map((response, index) => {
+					const fecha = new Date(response?.date);
+					const dia = fecha.getDate();
+					const mes = fecha.getMonth() + 1;
+					return (
+						<h3 key={index} onClick={() => handleToCheckAutoRegistro(response?._id)}>
+							{`Registro dia ${dia}/${mes}`}
+						</h3>
+					);
+				})}
+			</div>
+		</section>
+	) : (
+		<section className='sectionNoResponses'>
+			<h2>Conoce tus autoregistros</h2>
+			<h3>Parece que aun no has hecho ningun autoregistro</h3>
+		</section>
+	);
+};
 
 export default AutoRegistroFilters;
--- src/components/AutoRegistroRender/AutoRegistroRender.jsx
+++ src/components/AutoRegistroRender/AutoRegistroRender.jsx
@@ -1,119 +1,146 @@
-'use client'
-import './AutoRegistroRender.scss'
-import { useAutoRegistro } from '@/zustand/store/autoRegistroStore'
-import { CircularProgress,Modal, ModalContent } from '@nextui-org/react'
+'use client';
+import './AutoRegistroRender.scss';
+import { useAutoRegistro } from '@/zustand/store/autoRegistroStore';
+import { CircularProgress, Modal, ModalContent } from '@nextui-org/react';
 // import SliderAutoRegistro from '@/helpers/SliderAutoRegistro'
-import { useState, useEffect } from 'react'
-import { Slider } from '@nextui-org/react'
-import { getCookie } from 'cookies-next'
-import { toastError, toastSuccess } from '@/helpers/toast'
+import React, { useState, useEffect } from 'react';
+import { Slider } from '@nextui-org/react';
+import { getCookie } from 'cookies-next';
+import { toastError, toastSuccess } from '@/helpers/toast';
 
-const RenderAutoRegistro = ({isOpen, onOpen, onOpenChange, data, type}) => {
-    const {createResponseSR} = useAutoRegistro()
-    const userId = getCookie('jsdklfsdjklfdsjfds');
-    const [userResponses, setUserResponses] = useState([]);
-    const [comments, setComments] = useState("")
-    
-    const excersice = data.filter((section) => section.type === type)
-    
-    useEffect(() => {
-        // if(excersice) {
-        //     if(Object.keys(excersice).length === 0){
-        //     const data = getAutoRegistro(id); // Llama a getAutoRegistro de manera síncrona
-        // }  
-        // }
-        
-        if (excersice?.questions) {
-            setUserResponses(excersice?.questions?.map(() => 3));
-        }
-    }, [excersice]);
+const RenderAutoRegistro = ({ isOpen, onOpen, onOpenChange, data, type }) => {
+	const { createResponseSR } = useAutoRegistro();
+	const userId = getCookie('jsdklfsdjklfdsjfds');
+	const [userResponses, setUserResponses] = useState([]);
+	const [comments, setComments] = useState('');
 
-    const handleChangeComments = (event) => {
-        setComments(event.target.value)
-    }
+	const excersice = data.filter(section => section.type === type);
 
-    const handleChangeResponse = (event, index) => {
-        const newResponses = [...userResponses];
-        newResponses[index] = event;
-        setUserResponses(newResponses); // Actualiza el estado de las respuestas del usuario
-        console.log(newResponses)
-    }
+	useEffect(() => {
+        console.log('excersice ', excersice);
 
-    const handleSaveResponse = () => {
-        console.log(comments)
-        try {
-            if(comments === ""){
-                throw new Error("Complete todos los campos");
-            }
-            const bodyAuto ={
-                selfRegisterId: excersice[0]._id,
-                userId,
-                response: userResponses,
-                comments
-            }
-            createResponseSR(bodyAuto)
-        } catch (error) {
-            toastError(error.message)
-        }
-    }
-    return (
-       excersice && Object.keys(excersice).length > 0 && (
-        <Modal className=''  isOpen={isOpen} onOpen={onOpen} onOpenChange={onOpenChange} size='5xl' backdrop='blur' scrollBehavior="inside">
-            <ModalContent className='modalContent'>
-                {onClose => (
-            <div className="mainAutoRender">
-                {excersice?.map((exer) =>
-                <>
-                <header>
-                    <h1>{exer?.name}</h1>
-                    <h2>{exer?.type}</h2>
-                </header>
-                <article>
-                     {exer.questions?.map((question, index) => (
-                        <section className="section1AutoRender" key={index}>
-                            <h3 className='autoRegistroQuestion'>{question?.selfQuestion}</h3>
-                            <h3 className='autoRegistroAgree'>{question?.agree}</h3>
-                            <Slider className='autoRegistroSlider'
-                            
-                            fillOffset={1}
-                            classNames={{
-                                base: "max-w-md gap-3",
-                         
-                              }}
-        onChange={(event) => handleChangeResponse(event, index)}
-        color={userResponses[index] === 3 ? "foreground" : userResponses[index] < 3 ? "danger" : "success"}
-        size="md"
-        step={1}
-        defaultValue={3}
-        showSteps={true} 
-        maxValue={5} 
-        minValue={1}
-        marks={[
-            {value : 1},
-            {value : 2},
-            {value : 3},
-            {value : 4},
-            {value : 5}
-        ]}
-        
-        />
-                            <h3 className='autoRegistroDisagree'>{question?.disagree}</h3>
-                        </section>
-                    ))}
-                    <textarea className='textAutoRegistro' onChange={handleChangeComments} name="" id="" cols="30" rows="10" placeholder='¿Por que te sentiste asi?'/>
-                </article>
-                </>)}
-                <footer>
-                    <button onClick={handleSaveResponse} className="buttonFooterAutoRender">
-                        Guardar Respuestas
-                    </button>
-                </footer>
-            </div>
-            )}
-            </ModalContent>
-            </Modal>
-        )
-    );
-}
+
+		if (excersice[0]?.questions) {
+			setUserResponses(excersice[0]?.questions?.map(() => 3));
+            console.log('userResponses ', userResponses);
+
+		}
+
+	}, [isOpen]);
+
+	const handleChangeComments = event => {
+		setComments(event.target.value);
+	};
+
+	const handleChangeResponse = (event, index) => {
+		const newResponses = [...userResponses];
+		newResponses[index] = event;
+		setUserResponses(newResponses); // Actualiza el estado de las respuestas del usuario
+		console.log(newResponses);
+	};
+
+	const handleSaveResponse = () => {
+		console.log(comments);
+		try {
+			if (comments === '') {
+				throw new Error('Complete todos los campos');
+			}
+			const bodyAuto = {
+				selfRegisterId: excersice[0]._id,
+				userId,
+				response: userResponses,
+				comments,
+			};
+			createResponseSR(bodyAuto);
+		} catch (error) {
+			toastError(error.message);
+		}
+	};
+	return (
+		excersice &&
+		Object.keys(excersice).length > 0 && (
+			<Modal
+				className=''
+				isOpen={isOpen}
+				onOpen={onOpen}
+				onOpenChange={onOpenChange}
+				size='5xl'
+				backdrop='blur'
+				scrollBehavior='inside'>
+				<ModalContent className='modalContent'>
+					{onClose => (
+						<div className='mainAutoRender'>
+							{excersice?.map( (exer , key) => (
+								<React.Fragment key={key}>
+									<header>
+										<h1>{exer?.name}</h1>
+										<h2>{exer?.type}</h2>
+									</header>
+									<article>
+										{exer.questions?.map((question, index) => (
+											<section className='section1AutoRender' key={index}>
+												<h3 className='autoRegistroQuestion'>
+													{question?.selfQuestion}
+												</h3>
+												<h3 className='autoRegistroAgree'>{question?.agree}</h3>
+												<Slider
+													className='autoRegistroSlider'
+													fillOffset={1}
+													classNames={{
+														base: 'max-w-md gap-3',
+													}}
+													onChange={event => handleChangeResponse(event, index)}
+													color={
+														userResponses[index] === 3
+															? 'foreground'
+															: userResponses[index] < 3
+															? 'danger'
+															: 'success'
+													}
+													size='md'
+													step={1}
+													defaultValue={3}
+													showSteps={true}
+													maxValue={5}
+													minValue={1}
+													marks={[
+														{ value: 1 },
+														{ value: 2 },
+														{ value: 3 },
+														{ value: 4 },
+														{ value: 5 },
+													]}
+												/>
+												<h3 className='autoRegistroDisagree'>
+													{question?.disagree}
+												</h3>
+											</section>
+										))}
+										<textarea
+											className='textAutoRegistro'
+											onChange={handleChangeComments}
+											name=''
+											id=''
+											cols='30'
+											rows='10'
+											placeholder='¿Por que te sentiste asi?'
+										/>
+									</article>
+								</React.Fragment>
+							))}
+							<footer>
+								<button
+									onClick={handleSaveResponse}
+									className='buttonFooterAutoRender'>
+									Guardar Respuestas
+								</button>
+							</footer>
+						</div>
+					)}
+				</ModalContent>
+			</Modal>
+		)
+	);
+};
 
 export default RenderAutoRegistro;
